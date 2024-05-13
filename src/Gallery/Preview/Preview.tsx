96bdf4a3735fe2e4597154034aa58a047ffdd6b5
Frackenbock <rasaw1992@gmail.com>
2024-05-13 09:51:44 +0300

    modified:   src/Gallery/Gallery.module.scss 	modified:   src/Gallery/Preview/Preview.tsx

--- src/Gallery/Preview/Preview.tsx
+++ src/Gallery/Preview/Preview.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{useEffect, useRef,useMemo} from "react"
 import { CommonClassProps, Photo } from "../types"
 import styles from '../Preview/Preview.module.scss'
 
@@ -6,28 +6,46 @@ interface PreviewProps extends CommonClassProps{
     photos:Photo[];
     activeIndexPhoto:number;
 }
-
 export const Preview:React.FC<PreviewProps>=({photos,activeIndexPhoto,className})=>{
+
+    if(photos.length===0){
+        return null;
+    }
+
+    const previewCont = useRef<HTMLUListElement>(null)
+
+    useEffect(()=>{
+        if(!previewCont.current){
+            return
+        }
+        previewCont.current.style.transform = `translate3d(-${activeIndexPhoto * 164}px, 0, 0 )`
+    },[activeIndexPhoto])
+
     return(
         <div  className={[className,styles.previewGallery].join(' ')}>
-            <ul className={styles.previewGalleryTrack}>
-                {
-                    photos.map((photo)=>{
-                        return(
-                            <li key={photo.id} className={styles.previewGalleryPreview}>
-                                <img 
-                                    src={photo.src} 
-                                    alt={photo.description}
-                                    className={styles.previewGalleryImage}
-                                />
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-            <div className={styles.previewGalleryCover}>
-                {activeIndexPhoto+1}/{photos.length}
-            </div>
+                {useMemo(()=>
+                    (
+                        <ul ref={previewCont} className={styles.previewGalleryTrack}>
+                            {
+                                photos.map((photo)=>{
+                                    return(
+                                        <li key={photo.id} className={styles.previewGalleryPreview} >
+                                            <img 
+                                                src={photo.src} 
+                                                alt={photo.description}
+                                                className={styles.previewGalleryImage}
+                                            />
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    )
+                ,[])}
+                
+                <div className={styles.previewGalleryCover}>
+                    {activeIndexPhoto+1}/{photos.length}
+                </div>
         </div>
     )
 }   
